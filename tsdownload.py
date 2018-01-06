de1648a51e57f8860f98802b2400ec017b5c940c
sam46 <abdlwahdsa@gmail.com>
2018-01-06 00:18:03 -0500

    init commit

---
+++ tsdownload.py
@@ -0,0 +1,59 @@
+""" download segmented mpeg video (.ts files) from streaming websites """
+import sys
+import requests
+
+M3URL = ""  # fill in: .m3u8 file url
+SEG1URL = ""  # fill in: first ts segment file url
+REFERER = ""  # fill in: some websites require a specific referer website to allow the request
+
+OUTNAME = 'video.ts'  # default output file name
+LOC = "X:/Downloads/Video/"  # default save location
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:57.0) Gecko/20100101 Firefox/57.0",
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Referer": REFERER,
+    "DNT": "1",
+    "Connection": "keep-alive",
+    "Pragma": "no-cache",
+    "Cache-Control": "no-cache"
+}
+
+
+def getSegsNum(m3):
+    """ figure out how many segments there are using the m3u8 file """
+    lines = m3.text.split('\n')
+    nsegs = 0
+    for line in lines:
+        if 'seg-' in line and '.ts' in line:
+            tokens = line.split('-')
+            idx = 0
+            for i, tok in enumerate(tokens):
+                if '/seg' in tok:
+                    idx = i + 1
+                    break
+            nsegs = int(tokens[idx])
+    return nsegs
+
+
+def dumpSegs(initUrl, n, path):
+    """ downlaod and combine the .ts files
+    given the first seg's url, the number of segments and
+    the destination download path """
+    with open(path, 'wb') as f:
+        for i in range(1, n + 1):
+            segurl = initUrl.replace('seg-1-', 'seg-{:d}-'.format(i))
+            seg = requests.get(segurl, headers=HEADERS)
+            f.write(seg.content)
+            print('dumped seg%d.ts' % i, '  %d%%' % (i * 100 / n))
+
+
+if __name__ == "__main__":
+    PATH = LOC + OUTNAME
+    if len(sys.argv) > 1:
+        PATH = sys.argv[1]
+    m3u8 = requests.get(M3URL, headers=HEADERS)
+    nsegs = getSegsNum(m3u8)
+    dumpSegs(SEG1URL, nsegs, PATH)
